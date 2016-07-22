6019d89b5445b6f7c3811b464e391a1b40f334e3
Susanth Sampath <sampath9dasari@gmail.com>
2016-07-22 15:25:17 +0530

    temp index page

--- sendemail.php
+++ sendemail.php
@@ -5,13 +5,13 @@
 		'message'=>'Thank you for contact us. As early as possible  we will contact you '
 	);
 
-    $name = @trim(stripslashes($_POST['name'])); 
-    $email = @trim(stripslashes($_POST['email'])); 
-    $subject = @trim(stripslashes($_POST['subject'])); 
-    $message = @trim(stripslashes($_POST['message'])); 
+    $name = @trim(stripslashes($_POST['name']));
+    $email = @trim(stripslashes($_POST['email']));
+    $subject = @trim(stripslashes($_POST['subject']));
+    $message = @trim(stripslashes($_POST['message']));
 
     $email_from = $email;
-    $email_to = 'email@email.com';//replace with your email
+    $email_to = 'sampath9dasari@gmail.com';//replace with your email
 
     $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
@@ -20,5 +20,5 @@
     echo json_encode($status);
     die;
 
-	
+
 ?>
