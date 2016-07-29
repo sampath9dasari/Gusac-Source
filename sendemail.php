3d438360a75cf66a265b10a8aede5d981dabbd94
Susanth Sampath <sampath9dasari@gmail.com>
2016-07-29 11:09:36 +0530

    Final Home

--- sendemail.php
+++ sendemail.php
@@ -2,7 +2,7 @@
 	header('Content-type: application/json');
 	$status = array(
 		'type'=>'success',
-		'message'=>'Thank you for contact us. As early as possible  we will contact you '
+		'message'=>'Thank you for contacting us. We will get back to you as early as possible'
 	);
 
     $name = @trim(stripslashes($_POST['name']));
@@ -11,7 +11,7 @@
     $message = @trim(stripslashes($_POST['message']));
 
     $email_from = $email;
-    $email_to = 'sampath9dasari@hotmail.com';//replace with your email
+    $email_to = 'gusac@gitam.edu';//replace with your email
 
     $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
